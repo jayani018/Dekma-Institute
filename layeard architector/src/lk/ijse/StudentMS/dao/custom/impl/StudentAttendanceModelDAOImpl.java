b6a0c18aa86ad4da845cfe9d0113ab39c0800860
jayani018 <jayaniarunika112gmail.com>
2023-01-29 00:58:28 +0530

    add StudentAttendanceModelDAOImpl class methods

--- layeard architector/src/lk/ijse/StudentMS/dao/custom/impl/StudentAttendanceModelDAOImpl.java
+++ layeard architector/src/lk/ijse/StudentMS/dao/custom/impl/StudentAttendanceModelDAOImpl.java
@@ -1,25 +1,33 @@
 package lk.ijse.StudentMS.dao.custom.impl;
 
+import lk.ijse.StudentMS.dao.SqlUtil;
 import lk.ijse.StudentMS.dao.custom.StudentAttendanceModelDAO;
 import lk.ijse.StudentMS.model.StudentAttendanceDTO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class StudentAttendanceModelDAOImpl implements StudentAttendanceModelDAO {
     @Override
     public ArrayList<StudentAttendanceDTO> getAll() throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<StudentAttendanceDTO> studentAttendance = new ArrayList<>();
+        ResultSet rst = SqlUtil.execute("SELECT * FROM student_attendance");
+        while (rst.next()){
+            StudentAttendanceDTO studentAttendanceDTO = new StudentAttendanceDTO(rst.getString("SID"), rst.getString("date"), rst.getString("time"));
+            studentAttendance.add(studentAttendanceDTO);
+        }
+        return studentAttendance;
     }
 
     @Override
-    public boolean add(StudentAttendanceDTO entity) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean add(StudentAttendanceDTO dto) throws SQLException, ClassNotFoundException {
+       return SqlUtil.execute("INSERT INTO student_attendance VALUES (?,?,?)",dto.getSID(),dto.getDate(),dto.getTime());
     }
 
     @Override
-    public boolean update(StudentAttendanceDTO entity) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean update(StudentAttendanceDTO dto) throws SQLException, ClassNotFoundException {
+        return SqlUtil.execute("UPDATE student_attendance SET date=?,time=?  WHERE=SID?",dto.getDate(),dto.getTime(),dto.getSID());
     }
 
     @Override
@@ -34,7 +42,7 @@ public class StudentAttendanceModelDAOImpl implements StudentAttendanceModelDAO 
 
     @Override
     public boolean delete(String id) throws SQLException, ClassNotFoundException {
-        return false;
+       return SqlUtil.execute("DELETE FROM student_attendance  WHERE SID=?",id);
     }
 
     @Override
