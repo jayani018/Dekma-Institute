f33c40a3c15e04be2590d8362d498b263195ab05
jayani018 <jayaniarunika112gmail.com>
2023-01-28 21:26:14 +0530

    add PaymentsModelDAOImpl class methods

--- layeard architector/src/lk/ijse/StudentMS/dao/custom/impl/PaymentsModelDAOImpl.java
+++ layeard architector/src/lk/ijse/StudentMS/dao/custom/impl/PaymentsModelDAOImpl.java
@@ -1,8 +1,10 @@
 package lk.ijse.StudentMS.dao.custom.impl;
 
+import lk.ijse.StudentMS.dao.SqlUtil;
 import lk.ijse.StudentMS.dao.custom.PaymentsModelDAO;
 import lk.ijse.StudentMS.model.PaymentsDTO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,17 +13,26 @@ public class PaymentsModelDAOImpl implements PaymentsModelDAO {
 
     @Override
     public ArrayList<PaymentsDTO> getAll() throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<PaymentsDTO> Payments = new ArrayList<>();
+        ResultSet rst = SqlUtil.execute("SELECT * FROM Payments");
+        while (rst.next()){
+            PaymentsDTO paymentsDTO = new PaymentsDTO(rst.getString("SID"), rst.getString("CarID"), rst.getString("amount"), rst.getString("payment_months"), rst.getString("time"));
+            Payments.add(paymentsDTO);
+
+        }
+        return Payments;
+
     }
 
     @Override
-    public boolean add(PaymentsDTO entity) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean add(PaymentsDTO dto) throws SQLException, ClassNotFoundException {
+        return SqlUtil.execute("INSERT INTO Payments VALUES (?,?,?,?,?)",dto.getSID(),dto.getCarID(),dto.getAmount(),dto.getPayment_months(),dto.getTime() );
     }
 
     @Override
-    public boolean update(PaymentsDTO entity) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean update(PaymentsDTO dto) throws SQLException, ClassNotFoundException {
+        return SqlUtil.execute("UPDATE Payments SET SID=?, cardNo=?,amount=?, payment_months=?,time=? WHERE SId=?", dto.getCarID(),dto.getAmount(),dto.getPayment_months(),dto.getTime(),dto.getSID());
+
     }
 
     @Override
@@ -36,11 +47,16 @@ public class PaymentsModelDAOImpl implements PaymentsModelDAO {
 
     @Override
     public boolean delete(String id) throws SQLException, ClassNotFoundException {
-        return false;
+        return SqlUtil.execute("DELETE FROM Payments  WHERE SId=?",id);
     }
 
     @Override
     public PaymentsDTO search(String id) throws SQLException, ClassNotFoundException {
-        return null;
+        ResultSet rst = SqlUtil.execute("SELECT * FROM Batch WHERE BID=?", id);
+       PaymentsDTO paymentsDTO = null;
+        if (rst.next()) {
+           paymentsDTO = new PaymentsDTO(id, rst.getString("CarID"), rst.getString("amount"), rst.getString("payment_months"), rst.getString("time"));
+        }
+        return paymentsDTO;
     }
 }
