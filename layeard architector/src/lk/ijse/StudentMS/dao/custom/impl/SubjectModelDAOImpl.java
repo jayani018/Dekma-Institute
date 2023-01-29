d73889bc6b317726b83456ff0eb3ae7e39ac6030
jayani018 <jayaniarunika112gmail.com>
2023-01-29 21:17:30 +0530

    add SubjectModelDAOImpl class methode

--- layeard architector/src/lk/ijse/StudentMS/dao/custom/impl/SubjectModelDAOImpl.java
+++ layeard architector/src/lk/ijse/StudentMS/dao/custom/impl/SubjectModelDAOImpl.java
@@ -1,8 +1,10 @@
 package lk.ijse.StudentMS.dao.custom.impl;
 
+import lk.ijse.StudentMS.dao.SqlUtil;
 import lk.ijse.StudentMS.dao.custom.SubjectModelDAO;
 import lk.ijse.StudentMS.model.SubjectDTO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -12,17 +14,23 @@ public class SubjectModelDAOImpl implements SubjectModelDAO {
 
     @Override
     public ArrayList<SubjectDTO> getAll() throws SQLException, ClassNotFoundException {
-        return null;
+      ArrayList<SubjectDTO> Subject= new ArrayList<>();
+        ResultSet rst = SqlUtil.execute("SELECT * FROM Subject");
+        while (rst.next()){
+            SubjectDTO subjectDTO = new SubjectDTO(rst.getString("SUBID"), rst.getString("SubName"));
+            Subject.add(subjectDTO);
+        }
+        return Subject;
     }
 
     @Override
-    public boolean add(SubjectDTO entity) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean add(SubjectDTO dto) throws SQLException, ClassNotFoundException {
+       return SqlUtil.execute("INSERT INTO Subject VALUES (?,?)",dto.getSUBID(),dto.getSubName());
     }
 
     @Override
-    public boolean update(SubjectDTO entity) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean update(SubjectDTO dto) throws SQLException, ClassNotFoundException {
+        return SqlUtil.execute("UPDATE Subject SET SubName=?  WHERE SUBID=?",dto.getSubName(),dto.getSUBID());
     }
 
     @Override
@@ -37,11 +45,16 @@ public class SubjectModelDAOImpl implements SubjectModelDAO {
 
     @Override
     public boolean delete(String id) throws SQLException, ClassNotFoundException {
-        return false;
+       return SqlUtil.execute("DELETE FROM Subject  WHERE SUBID=?",id);
     }
 
     @Override
     public SubjectDTO search(String id) throws SQLException, ClassNotFoundException {
-        return null;
+        ResultSet rst = SqlUtil.execute("SELECT * FROM Subject WHERE SUBID=?", id);
+        SubjectDTO subjectDTO=null;
+        if (rst.next()){
+             subjectDTO = new SubjectDTO(id, rst.getString("SubName"));
+        }
+        return subjectDTO;
     }
 }
